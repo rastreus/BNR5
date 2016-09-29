c3ed5ba2ce998292e50462b6d28c0f191e917c21
Russell Dillin <rastreus@gmail.com>
2016-09-28 22:54:09 -0700

    Chapter 1: A Simple iOS Application

---
+++ QuizBNR/QuizBNR/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  QuizBNR
+//
+//  Created by Russell Dillin on 9/28/16.
+//  Copyright © 2016 rastreus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = ["From what is cognac made?",
+                               "What is 7+7",
+                               "What is the capital of Vermont?"]
+    let answers: [String] = ["Grapes",
+                             "14",
+                             "Montpelier"]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        // 💥 '++' is unavailable: it has been removed in Swift 3
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+}
+
